1076a9a9032a4d6afacd6ba99b9153ad5c63e844
Anastasia <nstanastasia25@yandex.ru>
2022-04-18 21:00:23 +0300

    change  ex002

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int numberA = 3;
+int numberB = 2;
+int max = numberA;
+if (numberB>max) max=numberB;
+Console.Write("max=");
+Console.WriteLine(max);
--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int numberA = 3;
+int numberB = 2;
+int max = numberA;
+if (numberB>max) max=numberB;
+Console.Write("max=");
+Console.WriteLine(max);
