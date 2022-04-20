d0fc438787e8257965bfe4954bbfc00b08dd6f29
Anastasia <nstanastasia25@yandex.ru>
2022-04-20 16:49:40 +0300

    add  ex005

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+ Console.Write("Введите N");
+double  N = Convert.ToDouble(Console.ReadLine());
+double [] massiv = {-N,N} ;
+if (-N > massiv > N) 
+{
+    Console.WriteLine (massiv);
+}
+else 
+{
+massiv = massiv + 1 ;
+}
---
+++ ex005/markdown5.md
@@ -0,0 +1,1 @@
+![ex005](Screenshot_1.png)
