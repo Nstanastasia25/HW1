13e94bf56cd196c571e91face63644ec61b47345
Anastasia <nstanastasia25@yandex.ru>
2022-04-25 20:38:11 +0300

    change ex005

--- ex005/Program.cs
+++ ex005/Program.cs
@@ -1,6 +1,8 @@
-﻿//  Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
- Console.Write("Введите N ");
-double  N = Convert.ToDouble(Console.ReadLine());
+﻿//  Напишите программу, которая на вход принимает одно число (N), а на выходе показывает  
+//все целые числа в промежутке от -N до N.
+
+Console.Write("Введите N ");
+double N = Convert.ToDouble(Console.ReadLine());
 double A = -N + 1 ;
 while (A>-N || A<N) 
 {
